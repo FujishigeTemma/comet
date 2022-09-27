faa7200c28b4ad899143d33c088179c988226606
ryoha000 <the7uya@gmail.com>
2022-09-27 14:11:37 +0900

    add comment

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -5,6 +5,7 @@ import { getPageImgUrl, mustConvertToIntNumber } from '/@/utils'
 export const Page = () => {
   const { comicId, volumeId, pageId } = useParams()
 
+  // MEMO: 範囲外に飛ぶことあるけどまぁええか
   return (
     <div className="flex w-full h-screen">
       <Link to={`/comics/${comicId}/volumes/${volumeId}/pages/${mustConvertToIntNumber(pageId) - 1}`} className="w-1/2">
