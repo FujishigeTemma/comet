078f3e08516db5cad5563110f711ea6f100e8f9f
ryoha000 <the7uya@gmail.com>
2022-09-27 14:01:48 +0900

    sidebar 中身

--- src/components/Nav/Sidebar.tsx
+++ src/components/Nav/Sidebar.tsx
@@ -1,11 +1,28 @@
+import { GoGraph } from 'react-icons/go'
+import { TbBooks } from 'react-icons/tb'
+import { WiMeteor } from 'react-icons/wi'
+import { Link, useParams } from 'react-router-dom'
+
 export const Sidebar = () => {
+  const { comicId } = useParams()
   return (
-    <div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
+    <div className="py-4 px-8">
+      <Link to={'/'} className="flex gap-2 items-center">
+        <WiMeteor size={24} />
+        <div className="text-xl font-bold">comet</div>
+      </Link>
+      <div className="p-2 flex flex-col gap-2">
+        <Link to={'/comics'} className="flex gap-2 items-center">
+          <TbBooks />
+          <div>comics</div>
+        </Link>
+        {comicId ? (
+          <Link to={`/comics/${comicId}/volumes/metrics`} className="flex gap-2 items-center">
+            <GoGraph />
+            <div>metrics</div>
+          </Link>
+        ) : null}
+      </div>
     </div>
   )
 }
