483d5a5433718ecaaa4b13ef92a2a415a9bc80db
ryoha000 <the7uya@gmail.com>
2022-09-27 12:21:55 +0900

    fix

--- src/components/Comic/ComicComponent.tsx
+++ src/components/Comic/ComicComponent.tsx
@@ -13,7 +13,7 @@ export const ComicComponent = ({ comicsId, comic }: Props) => {
       <Link to={`/comics/${comicsId}/volumes/${comic.id}`}>
         <img src={`/comics-meta/${comicsId}/${comic.id}/${comic.cover}.jpg`} className="w-full" />
         <div className="absolute flex items-center justify-center group inset-0 z-10 hover:bg-opacity-70 bg-gray-900 bg-opacity-0 transition">
-          <div className="opacity-0 group-hover:opacity-100 transition text-gray-100">{comic.name}</div>
+          <div className="opacity-0 group-hover:opacity-100 transition text-gray-100 font-bold">{comic.name}</div>
         </div>
       </Link>
     </div>
