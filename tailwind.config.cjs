65783cb37233d857f82297d523ae2f28fd631138
sapphi-red <green@sapphi.red>
2022-09-28 14:44:26 +0900

    テーマの変更

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,20 +17,8 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     themes: [
-      {
-        winter: {
-          primary: '#66CC8A',
-          secondary: '#377CFB',
-          accent: '#EA5234',
-          neutral: '#333C4D',
-          'base-100': '#FFFFFF',
-          info: '#3ABFF8',
-          success: '#36D399',
-          warning: '#FBBD23',
-          error: '#F87272'
-        }
-      }
+      'light'
     ],
-    darkTheme: 'winter'
+    darkTheme: 'light'
   }
 }
