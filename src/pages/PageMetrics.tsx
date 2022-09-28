4f8bbc089bc3e51aa52d07a7ab742ffea98274c4
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2022-09-28 15:12:56 +0900

    :zap:見た目

--- src/pages/PageMetrics.tsx
+++ src/pages/PageMetrics.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useMemo, useState } from 'react'
+import { BiRun } from 'react-icons/bi'
+import { FaRegEye } from 'react-icons/fa'
 import { useParams } from 'react-router-dom'
 
 import { PageComponent } from '/@/components/Page/PageComponent'
@@ -29,6 +31,27 @@ const gazeInfoToSvgPathD = (gazeInfo: GazeInfo, showingTime: number): string => 
   return d
 }
 
+const StatusChip = () => (
+  <div className="stats shadow">
+    <div className="stat">
+      <div className="stat-title">累積離脱率</div>
+      <div className="stat-value text-primary">23.4%</div>
+      <div className="stat-figure text-primary">
+        <BiRun size={32} className="m-auto"></BiRun>
+      </div>
+      <div className="stat-desc text-accent">+2.8%</div>
+    </div>
+    <div className="stat">
+      <div className="stat-title">平均滞在時間</div>
+      <div className="stat-value text-secondary">28.3秒</div>
+      <div className="stat-figure text-secondary">
+        <FaRegEye size={32} className="m-auto"></FaRegEye>
+      </div>
+      <div className="stat-desc text-accent">文字数が多すぎる可能性があります</div>
+    </div>
+  </div>
+)
+
 export const PageMetrics = () => {
   const { comicId, volumeId, pageId } = useParams()
   const gazeInfo = useGazeInfo({ comicId, volumeId, pageId })
@@ -43,42 +66,48 @@ export const PageMetrics = () => {
   }, [gazeMaxTime])
 
   return (
-    <div className="flex flex-col justify-center w-full h-screen gap-4">
-      <div className="flex gap-4">
-        <div className="form-control">
-          <label className="label cursor-pointer">
-            <input type="checkbox" className="toggle toggle-primary" checked={showGaze} onChange={() => setShowGaze(!showGaze)} />
-            <span className="label-text ml-2">視線の表示</span>
-          </label>
+    <div className="flex justify-center items-center w-full h-screen py-4">
+      <div className="flex flex-col justify-center items-center gap-2 w-full max-w-4xl h-full">
+        <div className="flex gap-8 w-full">
+          <div className="flex gap-4 self-start justify-start">
+            <div className="form-control">
+              <label className="label cursor-pointer gap-2">
+                <span className="label-text ml-2">視線の表示</span>
+                <input type="checkbox" className="toggle toggle-primary" checked={showGaze} onChange={() => setShowGaze(!showGaze)} />
+              </label>
+            </div>
+          </div>
+          {showGaze && (
+            <div className="flex flex-1 gap-4">
+              <label className="label flex-1">
+                <span className="label-text mr-2 whitespace-nowrap">時間</span>
+                <input type="range" min="0" max={gazeMaxTime} value={showingTime} onChange={e => setShowingTime(e.target.valueAsNumber)} className="range" />
+              </label>
+            </div>
+          )}
         </div>
-        <div className="text-sm flex items-center">離脱率: 23.4%</div>
-        <div className="text-sm flex items-center">平均滞在時間: 12.3秒</div>
-      </div>
-      {showGaze && (
-        <div className="flex gap-4">
-          <label className="label flex-1">
-            <span className="label-text mr-2 whitespace-nowrap">時間</span>
-            <input type="range" min="0" max={gazeMaxTime} value={showingTime} onChange={e => setShowingTime(e.target.valueAsNumber)} className="range" />
-          </label>
+
+        <div className="flex min-h-0 relative max-w-full">
+          <PageComponent
+            className="flex-1"
+            comicId={mustConvertToIntNumber(comicId)}
+            volumeId={mustConvertToIntNumber(volumeId)}
+            pageId={mustConvertToIntNumber(pageId)}
+            withLink={false}
+          />
+          <div>
+            {showGaze &&
+              gazeInfo.map((line, i) => (
+                <div key={i} className="absolute inset-0">
+                  <svg viewBox="0 0 1476.92 1050" xmlns="http://www.w3.org/2000/svg">
+                    <path d={gazeInfoToSvgPathD(line, showingTime)} fill="transparent" stroke="rgba(255,0,0,0.3)" strokeWidth="3" />
+                  </svg>
+                </div>
+              ))}
+          </div>
         </div>
-      )}
-      <div className="flex min-h-0 relative">
-        <PageComponent
-          className="flex-1"
-          comicId={mustConvertToIntNumber(comicId)}
-          volumeId={mustConvertToIntNumber(volumeId)}
-          pageId={mustConvertToIntNumber(pageId)}
-          withLink={false}
-        />
-        <div>
-          {showGaze &&
-            gazeInfo.map((line, i) => (
-              <div key={i} className="absolute inset-0">
-                <svg viewBox="0 0 1476.92 1050" xmlns="http://www.w3.org/2000/svg">
-                  <path d={gazeInfoToSvgPathD(line, showingTime)} fill="transparent" stroke="rgba(255,0,0,0.3)" strokeWidth="3" />
-                </svg>
-              </div>
-            ))}
+        <div className="self-end">
+          <StatusChip />
         </div>
       </div>
     </div>
